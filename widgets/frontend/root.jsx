9838f799ebbbae5f20c432e66de6348f163fc4eb
Grace Patuwo <grace.patuwo@gmail.com>
2017-05-01 22:12:53 -0700

    root component established

---
+++ widgets/frontend/root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Root extends React.Component {
+    constructor(props){
+      super(props);
+    }
+
+    render(){
+      return (
+        <div>
+          <h1>hi there! welcome to root</h1>
+
+        </div>
+      );
+    }
+}
+
+export default Root;
