56f8446f27c243f7a73d6992dc52679e4b0455e9
Grace Patuwo <grace.patuwo@gmail.com>
2017-05-09 16:06:51 -0700

    added click handler

--- widgets/frontend/autocomplete.jsx
+++ widgets/frontend/autocomplete.jsx
@@ -6,7 +6,8 @@ class Autocomplete extends React.Component {
     this.state = {inputVal: ''};
 
     this.handleInput = this.handleInput.bind(this);
-    this.matches = this.matches.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    // this.matches = this.matches.bind(this);
   }
 
   handleInput(e){
@@ -17,22 +18,27 @@ class Autocomplete extends React.Component {
     return this.props.names.filter( el => el.toLowerCase().includes(val));
   }
 
+  handleClick(e){
+    let name = e.currentTarget.innerHTML;
+    this.setState({inputVal: name});
+  }
+
   render(){
     let searchResults = <div></div>;
     // display all names if no search or deletes search
     if (this.state.inputVal === '') {
       searchResults = this.props.names.map( (el, idx) =>
-        <li className='name' key={idx}>{el}</li>);
+        <li className='name' key={idx} onClick={this.handleClick}>{el}</li>);
     } else {
       // return what matches with input
-      searchResults = this.matches(this.state.inputVal).map( (el, idx) =>
-        <li className='name' key={idx}>{el}</li>);
+      searchResults = this.matches(this.state.inputVal.toLowerCase()).map( (el, idx) =>
+        <li className='name' key={idx} onClick={this.handleClick}>{el}</li>);
     }
 
     return (
       <div className='wrapper'>
         <div className='section-div'>
-          <h2>Autocomplete Names!</h2>
+          <h2>Autocomplete Search!</h2>
           <input onChange={this.handleInput}
              value={this.state.inputVal} placeholder='Search...'></input>
           <div id='results'>
