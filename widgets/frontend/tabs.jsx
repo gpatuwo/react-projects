1f826900bc1e0a77620afabdda1e9f0eec4271fe
Grace Patuwo <grace.patuwo@gmail.com>
2017-05-09 17:12:11 -0700

    added tab component

---
+++ widgets/frontend/tabs.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render(){
+    let tabs = this.props.tabs.map((tab, idx) =>
+    <li key={idx}
+      onClick={this.props.onTabClick.bind(null, idx)}
+      className='tab-header'>
+      {tab.title}
+    </li>);
+
+    return(
+      <ul className='tab-headers'>
+        {tabs}
+      </ul>
+    );
+  }
+}
+
+class Tabs extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {selectedTab: 0};
+
+    this.selectTab = this.selectTab.bind(this);
+  }
+
+  selectTab(tabIdx){
+    this.setState({selectedTab: tabIdx});
+  }
+
+  render(){
+
+    let selectedTab = this.props.tabs[this.state.selectedTab];
+
+    return (
+      <div className='tabs'>
+        <Header
+          selectedTab={selectedTab}
+          onTabClick={this.selectTab}
+          tabs={this.props.tabs}
+        />
+        <div className='tab-content'>
+          <article>
+            {selectedTab.content}
+          </article>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tabs;
--- widgets/frontend/widgets.jsx
+++ widgets/frontend/widgets.jsx
@@ -4,8 +4,15 @@ import ReactDOM from 'react-dom';
 import Clock from './clock';
 import Weather from './weather';
 import Autocomplete from './autocomplete';
+import Tabs from './tabs';
 
 const Names = ['Aviva', 'Zia', 'Iola', 'Carmine', 'Aza', 'Snax', 'Doner Kebab'];
+
+const TabsContent = [
+  {title: 'One', content: 'I am the first'},
+  {title: 'Two', content: 'I am the second'},
+  {title: 'Three', content: 'I am the third'},
+];
 
 class Root extends React.Component {
     constructor(props){
@@ -19,6 +26,7 @@ class Root extends React.Component {
           <Clock />
           <Weather />
           <Autocomplete names={Names}/>
+          <Tabs tabs={TabsContent}/>
         </div>
       );
     }
